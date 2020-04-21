881769e4a40e1b9c47103f769d004554f863451e
Zhenyuan Lu <zhenyuan.lu@gmail.com>
2020-04-20 21:36:28 -0400

    Added support for generalized ontology queries

--- lib/OntologyAPI/utils/config.py
+++ lib/OntologyAPI/utils/config.py
@@ -7,14 +7,9 @@ import functools
 @functools.lru_cache(maxsize=1)
 def get_config():
     config = {
-        're_url': os.environ.get('KBASE_ENDPOINT', 'https://ci.kbase.us/services').strip('/') + '/relation_engine_api',
-        'default_ns': 'go_ontology',
-        'ns': {
-            'go_ontology': 'GO'
-        }
+        're_url': os.environ.get('KBASE_ENDPOINT', 'https://ci.kbase.us/services').strip('/') + '/relation_engine_api'
     }
     return config
-
 
 class ConfigError(Exception):
     """Error initializing configuration."""
